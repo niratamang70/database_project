a62f0b34329c2325942f644d3d3eeed8fa3e4dfb
niratamang <neera678@gmail.com>
2025-04-08 21:26:11 -0500

    feat: create router in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,18 @@ import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import RecipePage from './pages/RecipePage';
 import RootLayout from './components/layout/RootLayout';
+import UsersPage from './pages/UsersPage';
+import RecipeDetailPage from './pages/RecipeDetailPage';
 
 const App = () => {
   return (
     <RootLayout>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="recipes" element={<RecipePage />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="recipes" element={<RecipePage />} />
+        <Route path="recipes/:id" element={<RecipeDetailPage />} />
+        <Route path="profile" element={<UsersPage />} />
+      </Routes>
     </RootLayout>
   );
 };
